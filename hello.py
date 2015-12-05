526f88e0d27168f6d43ee3ea4938ab331dc78698
Varun Ved <varunmved@gmail.com>
2015-12-04 18:59:37 -0800

    commiting html

--- hello.py
+++ hello.py
@@ -1,8 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect, flash, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/hello')
+def hello2():
+    return 'Hello from the other side'
+
+@app.route('/view')
+def view():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
